3118bea2b96dcae38e21b3c9794e25ffee0ba457
Thomas Bnt <thomasbnt@protonmail.com>
2023-01-02 14:44:31 +0100

    Moved the main file (index.js) and edited the code source

---
+++ src/index.js
@@ -0,0 +1,39 @@
+/*
+
+    Hello friend()
+    GitHub of Mr. Robøt : https://github.com/Mist3r-Robot
+    Website : https://mrrobot.app
+    — Thomas Bnt, Mr. Robøt 2022.
+
+    This file is used on the bot (https://mrrobot.app) for no cache files from CDNs. 
+    Discord uses CloudFlare for caching management which can be problematic when you change an image and display it on the platform. 
+    Even after a change on your side, Discord can still keep the old version in its cache.
+
+*/
+
+/**
+ * @param {number} suppressCacheVersion
+ */
+
+const suppressCacheVersion = '1'
+
+module.exports = {
+  /**
+   * @param {string} baseURL
+   * @param {string} file
+   * @param {boolean} log
+   * @param {boolean} disabled
+   */
+  getFolderIcons: function(baseURL, file, log, disabled) {
+    let genNumberCache
+    if (baseURL === null || !baseURL) return console.error('[suppressCache] Error : baseURL is not defined. Please define it.')
+    if (file === null || !file) return console.error('[suppressCache] Error : file is not defined. Please define it.')
+    if (!disabled) {
+      genNumberCache = Math.round(Math.random() * 0xffffffffff).toString(30)
+    } else {
+      genNumberCache = suppressCacheVersion
+    }
+    log ? console.log(`[suppressCache] Output : ${baseURL}/${file}?suppressCache=${genNumberCache}`) : null
+    return `${baseURL}/${file}?suppressCache=${genNumberCache}`
+  },
+}
---
+++ test/app.js
@@ -0,0 +1,8 @@
+const { getFolderIcons } = require("../src/index.js")
+
+getFolderIcons(
+  "https://mrrobot.app/Folder/Of/Icons",
+  "myFileName.png",
+  true,
+  false
+)
