bb6d59aaa0d469937061ca062d7c01da461fc4e3
Thomas Bnt <thomasbnt@protonmail.com>
2023-02-16 20:52:41 +0100

    :zap: Remove '/' at the end of baseURL if is present

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-/*
-
-    Hello friend()
-    GitHub of Mr. Robøt : https://github.com/Mist3r-Robot
-    Website : https://mrrobot.app
-    — Thomas Bnt, Mr. Robøt 2022.
-
-    This file is used on the bot (https://mrrobot.app) for no cache files from CDNs. 
-    Discord uses CloudFlare for caching management which can be problematic when you change an image and display it on the platform. 
-    Even after a change on your side, Discord can still keep the old version in its cache.
-
-*/
+/**
+ *   Hello friend()
+ *   GitHub of Mr. Robøt : https://github.com/Mist3r-Robot
+ *    Website : https://mrrobot.app
+ *    — Thomas Bnt, Mr. Robøt 2022.
+ *
+ *    This file is used on the bot (https://mrrobot.app) for no cache files from CDNs.
+ *    Discord uses CloudFlare for caching management which can be problematic when you change an image and display it on the platform.
+ *    Even after a change on your side, Discord can still keep the old version in its cache.
+ */
 
 /**
  * @param {number} suppressCacheVersion
  */
 
-const suppressCacheVersion = '1'
+const suppressCacheVersion = "1"
 
 module.exports = {
   /**
@@ -26,14 +24,11 @@ module.exports = {
    */
   getFolderIcons: function(baseURL, file, log, disabled) {
     let genNumberCache
-    if (baseURL === null || !baseURL) return console.error('[suppressCache] Error : baseURL is not defined. Please define it.')
-    if (file === null || !file) return console.error('[suppressCache] Error : file is not defined. Please define it.')
-    if (!disabled) {
-      genNumberCache = Math.round(Math.random() * 0xffffffffff).toString(30)
-    } else {
-      genNumberCache = suppressCacheVersion
-    }
+    if (baseURL === null || !baseURL) return console.error("[suppressCache] Error : baseURL is not defined. Please define it.")
+    if (file === null || !file) return console.error("[suppressCache] Error : file is not defined. Please define it.")
+    baseURL.endsWith("/") ? baseURL = baseURL.slice(0, -1) : baseURL
+    !disabled ? (genNumberCache = Math.round(Math.random() * 0xffffffffff).toString(30)) : genNumberCache = suppressCacheVersion
     log ? console.log(`[suppressCache] Output : ${baseURL}/${file}?suppressCache=${genNumberCache}`) : null
     return `${baseURL}/${file}?suppressCache=${genNumberCache}`
-  },
+  }
 }
